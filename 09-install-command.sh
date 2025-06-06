142fc2a175c7d25e84f6914992ecc306f066caa5
Nagashankar <nagashankar0848@gmail.com>
2025-06-06 21:07:02 +0530

    special-variables

--- 09-install-command.sh
+++ 09-install-command.sh
@@ -9,7 +9,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    exit 1 #give other than 0 upto 127
+    exit 1 #give other than 0 upto 127. If we give exit status, the shell will stop executing the other lines of commands if the previous line is having any error
 else
     echo "You are running with root access"
 fi
