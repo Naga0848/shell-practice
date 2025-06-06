72a81af6e765bdd33309d326deffe6868d89723f
Nagashankar <nagashankar0848@gmail.com>
2025-06-06 19:12:24 +0530

    special-variables

--- 09-install-command.sh
+++ 09-install-command.sh
@@ -1,6 +1,17 @@
 #!bin/bash
 
+
+USERID=(id -u)
 # I general we use id -u command in Linux to know the id of the user. But here in schell script we give this command to a variable called USERID.
 
+# if it is a root user, then id is equal to 0
 
+if [ $USERID -ne 0]
+then 
+    echo " Error :: Please run the script with the root access"
+else
+    echo " You are running with root access"
+fi
+
+dnf install mysql -y
 
