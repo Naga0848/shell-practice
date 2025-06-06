f3a8569c2e6dd62d07280770a75b8eb01ad80ea2
Nagashankar <nagashankar0848@gmail.com>
2025-06-06 19:20:32 +0530

    special-variables

--- 09-install-command.sh
+++ 09-install-command.sh
@@ -7,13 +7,10 @@ USERID=(id -u)
 # if it is a root user, then id is equal to 0
 
 if [ $USERID -ne 0 ]
-
-then 
-    echo "Error :: Please run the script with the root access"
-    exit 1 
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #give other than 0 upto 127
 else
-    echo " You are running with root access"
+    echo "You are running with root access"
 fi
-
-dnf install mysql
 
