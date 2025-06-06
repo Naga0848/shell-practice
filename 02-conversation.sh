1656d153b5bc3f747eeff633aca2413eefcd8ec1
Nagashankar <nagashankar0848@gmail.com>
2025-06-06 13:04:23 +0530

    conversation started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+echo "Trump :: Hi Musk, Good Morning"
+echo "Musk:: Hello Trump, Good Morning, I am doing great, How are you ?"
+echo "Trump:: I am fine thankyou, whats up ?"
+echo "Musk:: Nothing, just going to Mars now, will you come ?"
+echo "Trump:: Sorry, you carry on!! I will come once you come back "
