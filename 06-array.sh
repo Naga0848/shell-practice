493a0ddf09c4c6c1cf24f4cbcdd50c6958855f27
Nagashankar <nagashankar0848@gmail.com>
2025-06-06 14:26:46 +0530

    data-types

---
+++ 06-array.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "First Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
+
+echo "All movies: ${MOVIES[@]}"
+
+#movies is a variable
+
+#if we want to print the variable itself, we should use {} as shown in line no 9
+
