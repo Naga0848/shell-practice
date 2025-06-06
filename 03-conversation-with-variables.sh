44c1ebfae17676256406dfd4acda9ff37fed3c1f
Nagashankar <nagashankar0848@gmail.com>
2025-06-06 13:10:32 +0530

    conversation started

---
+++ 03-conversation-with-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+
+PERSON1=Trump   #there should be no space before and after =
+PERSON2=Musk    #there should be no space before and after =
+echo "PERSON1 :: Hi PERSON2, Good Morning"
+echo "PERSON2:: Hello PERSON1, Good Morning, I am doing great, How are you ?"
+echo "PERSON1:: I am fine thankyou, whats up ?"
+echo "PERSON2:: Nothing, just going to Mars now, will you come ?"
+echo "PERSON1:: Sorry, you carry on!! I will come once you come back"
