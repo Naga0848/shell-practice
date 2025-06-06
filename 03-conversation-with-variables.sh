083c50f6826d4763ee25b2748c5fc469a035b29b
Nagashankar <nagashankar0848@gmail.com>
2025-06-06 13:15:57 +0530

    conversation started

--- 03-conversation-with-variables.sh
+++ 03-conversation-with-variables.sh
@@ -3,8 +3,10 @@
 
 PERSON1=Trump   #there should be no space before and after =
 PERSON2=Musk    #there should be no space before and after =
-echo "PERSON1 :: Hi PERSON2, Good Morning"
-echo "PERSON2:: Hello PERSON1, Good Morning, I am doing great, How are you ?"
-echo "PERSON1:: I am fine thankyou, whats up ?"
-echo "PERSON2:: Nothing, just going to Mars now, will you come ?"
-echo "PERSON1:: Sorry, you carry on!! I will come once you come back"
+echo "$PERSON1 :: Hi $PERSON2, Good Morning"
+echo "$PERSON2:: Hello $PERSON1, Good Morning, I am doing great, How are you ?"
+echo "$PERSON1:: I am fine thankyou, whats up ?"
+echo "$PERSON2:: Nothing, just going to Mars now, will you come ?"
+echo "$PERSON1:: Sorry, you carry on!! I will come once you come back"
+
+#we have to use $ in variables so that shell will understand the difference between the normal text and a variable
