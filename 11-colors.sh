ceb28f2f02a700b5defcae87d6801c5317c477e4
Nagashankar <nagashankar0848@gmail.com>
2025-06-19 21:13:05 +0530

    functions

---
+++ 11-colors.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+# In general we represent success with green color and failure with red color in programming.
+
+# \e[<code>m   >>> is the syntax for the color in shell-scripting
+
+echo -e "\e[31m Hello-World \e[0m"
+
+echo "Hello NO Colors"
