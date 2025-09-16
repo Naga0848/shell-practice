175c00656d45728e60621b92abc4d548d6e2152d
Nagashankar <nagashankar0848@gmail.com>
2025-09-16 07:22:58 +0530

    shell-practice

--- again-pratcise.sh
+++ again-pratcise.sh
@@ -1,5 +1,16 @@
 #Again I am practising shell scripting
 
+
+
+USERID=$(id -u)
+
+if [USERDID -ne 0]
+then 
+    echo "please run the scripta as a root user"
+    else
+    echo "you are running as a root user"
+
+fi
 dnf list installed mqsql
 
 if [$? -eq 0 ]
