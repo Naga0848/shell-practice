1bb97ac2ecce59f71217747700aa8cc2ac8e43f5
Nagashankar <nagashankar0848@gmail.com>
2025-09-16 20:16:37 +0530

    shell-practice

--- before.sh
+++ before.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
-    echo "ERROR:: You are not runnign as a root user, please run with root access in order to install anything"
+    echo "ERROR:: You are not running as a root user, please run with root access in order to install anything"
     exit 1
 else
     echo "You are running as a root user"
