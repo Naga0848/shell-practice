02e0c5c02eb5beb97b65b096f02a8567eaf2717e
Nagashankar <nagashankar0848@gmail.com>
2025-09-17 22:26:20 +0530

    shell-practice

--- other.sh
+++ other.sh
@@ -9,23 +9,38 @@ then
 else
     echo "You are running as a root user"
 fi
+VALITADTE(){
+dnf install mysql -y 
+if [ $1 -eq 0 ]
+then
+    echo "$2 is installed properly"
+else
+    echo "$2 is not installed properly"
+fi
+}
 
 dnf list installed mysql -y 
 
 if [ $? -ne 0 ]
 then
     echo "Mysql is not installed, going to install"
-
-    dnf install mysql -y 
-    if [ $? -eq 0 ]
-    then
-        echo "mysql is installed properly"
-    else
-        echo "mysql is not installed properly"
-    fi
+    VALIDATE $? "Mysql"
 
 else
     echo "mysql is already installed...."
+    exit 1
+fi
+
+
+dnf list installed nginx -y 
+
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed, going to nginx"
+    VALIDATE $? "nginx"
+
+else
+    echo "nginx is already installed...."
     exit 1
 fi
 
