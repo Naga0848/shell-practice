685140c0ac7ecc5c56ae220353b3a21414e99b08
Nagashankar <nagashankar0848@gmail.com>
2025-09-17 21:10:38 +0530

    shell-practice

---
+++ after.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+
+then 
+    echo "You are not running as a root user"
+    exit 1
+
+else 
+    echo "Yo are running as a root user"
+
+
+
+    
