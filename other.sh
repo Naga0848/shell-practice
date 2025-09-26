6848fd3e4568c246f86c4d80fa1711415186963e
Nagashankar <nagashankar0848@gmail.com>
2025-09-26 08:49:41 +0530

    shell-practice

--- other.sh
+++ other.sh
@@ -1,44 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR :: Please execute script as a root user"
-    exit 1
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #give other than 0 upto 127
 else
-    echo "You are running as a root user"
+    echo "You are running with root access"
 fi
-VALITADTE(){
-dnf install mysql -y 
-if [ $1 -eq 0 ]
-then
-    echo "$2 is installed properly"
-else
-    echo "$2 is not installed properly"
-fi
-}
-
-dnf list installed mysql -y 
-
-if [ $? -ne 0 ]
-then
-    echo "Mysql is not installed, going to install"
-    VALIDATE $? "Mysql"
-
-else
-    echo "mysql is already installed...."
-fi
-
-
-dnf list installed nginx -y 
-
-if [ $? -ne 0 ]
-then
-    echo "nginx is not installed, going to nginx"
-    VALIDATE $? "nginx"
-else
-    echo "nginx is already installed...."
-    
-fi
-
