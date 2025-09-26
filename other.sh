6054a87ce2aaacfc80aa0e9b6412920c8c82f375
Nagashankar <nagashankar0848@gmail.com>
2025-09-26 09:29:15 +0530

    shell-practice

--- other.sh
+++ other.sh
@@ -31,3 +31,12 @@ else
     echo "nginx is already installed...Nothing to do"
 fi
 
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed...Nothing to do"
+fi
